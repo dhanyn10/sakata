1d9f99246efb3ef1761d34704ca1a5b71afe6944
Dhany Nurdiansyah <dhanyn.dhan@gmail.com>
2018-04-05 23:24:20 +0700

    add function word a

--- renderer.js
+++ renderer.js
@@ -1,3 +1,28 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+function word_a(type, length)
+{
+    result = null;
+    if(type == 1)
+    {
+        wval = "a";
+        for(a = length-4; a >= 1; a--)
+        {
+            if(length - a > a)
+            {
+                //lala
+            }
+            else
+            {
+
+            }
+            result += wval;
+        }
+    }
+}
+
+$(".button-function-1").click(() => {
+    valuefunction1 = $("#user-text").val();
+
+});
